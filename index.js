04028be3d1b32f109e18e1de4591c2a8cab9bc5e
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-14 18:12:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ const fi = (function () {
             }
             return result
           }
-            if (!callback) {
+          else if (!callback) {
                 for (let i = 0; i < array.length; i++) {
                     for (let j = i + 1; j < array.length; j++) {
                         if (array[i] === array[j]) {
